ac5a33445de19aea9d25fe2ae4fdde18a59d5734
Ejaz Ahmed <eahemdsiddiqui@gmail.com>
2025-07-16 12:42:12 +0530

    Image responsive, Index top

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-  function showMenu(menuId) {
-    document.querySelectorAll('.menu').forEach(menu => {
-      menu.classList.remove('active');
-    });
-    document.getElementById(menuId).classList.add('active');
-  }
+function toggleMenus() {
+    if(document.getElementById('menuA').classList.contains('hidden')){
+      document.getElementById('menuA').classList.remove('hidden');
+      document.getElementById('menuB').classList.add('hidden');
+      icon.className = 'fa-solid fa-arrow-right icon';
+
+    }else{
+      document.getElementById('menuA').classList.add('hidden');
+      document.getElementById('menuB').classList.remove('hidden');
+      icon.className = 'fa-solid fa-arrow-left icon';
+    }
+}
